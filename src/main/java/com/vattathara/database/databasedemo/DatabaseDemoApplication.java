ffe407c9b5c880b0a69d43dc45a4b5ac18ab60a5
Robinclear <robin.peter.vkm@gmail.com>
2020-07-27 20:04:39 -0300

    ERROR Because of NO NO Arg Constructor for the PERSON class

--- src/main/java/com/vattathara/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/vattathara/database/databasedemo/DatabaseDemoApplication.java
@@ -1,13 +1,29 @@
 package com.vattathara.database.databasedemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class DatabaseDemoApplication {
+import com.vattathara.database.databasedemo.jdbc.PersonJdbcDao;
 
+@SpringBootApplication
+public class DatabaseDemoApplication implements CommandLineRunner {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	PersonJdbcDao personJdbcDao;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseDemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("DATABASE All Person Table Data _-_-_>>> {}",personJdbcDao.findAll());
 	}
 
 }
