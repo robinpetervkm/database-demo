e94be21302e31dac227bd41365f3cc80939f26b0
Robinclear <robin.peter.vkm@gmail.com>
2020-08-14 16:53:55 -0300

    Package canged

--- src/main/java/com/in28Minutes/database/databasedemo/DatabaseDemoApplication.java
+++ src/main/java/com/in28Minutes/database/databasedemo/DatabaseDemoApplication.java
@@ -1,4 +1,4 @@
-package com.vattathara.database.databasedemo;
+package com.in28Minutes.database.databasedemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
